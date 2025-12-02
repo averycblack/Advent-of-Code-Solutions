af8a18c06a020da7c6fbbe0d52f5c88425071d62
Avery Black <avery.c.black@gmail.com>
2025-12-01 21:37:42 -0800

    2025: Day 2

--- 2025/src/days/day02.rs
+++ 2025/src/days/day02.rs
@@ -1,8 +1,56 @@
 use aoclib::solution::{Solution, SolutionPair};
 
-pub fn solve(_str: String) -> SolutionPair {
-    let sol1 = 0;
-    let sol2 = 0;
+fn invalid_p1(val: u64) -> bool {
+    let str = val.to_string();
+    if str.len() % 2 != 0 {
+        return false;
+    }
+
+    let (l, r) = str.split_at(str.len() / 2);
+    l == r
+}
+
+fn invalid_p2(val: u64) -> bool {
+    let str = val.to_string();
+    'outer: for len in 1..=(str.len() / 2) {
+        if str.len() % len != 0 {
+            continue;
+        }
+
+        let pattern = &str[0..len];
+        for i in (len..str.len()).step_by(len) {
+            if &str[i..(i + len)] != pattern {
+                continue 'outer;
+            }
+        }
+
+        return true;
+    }
+    false
+}
+
+pub fn solve(str: String) -> SolutionPair {
+    let (sol1, sol2) = str
+        .replace("\n", "")
+        .split(',')
+        .map(|s| {
+            let (l, r) = s.split_once('-').unwrap();
+            let l = l.parse().unwrap();
+            let r = r.parse().unwrap();
+            (l, r)
+        })
+        .fold((0, 0), |mut accum, (l, r)| {
+            for i in l..=r {
+                if invalid_p1(i) {
+                    accum.0 += i;
+                }
+
+                if invalid_p2(i) {
+                    accum.1 += i;
+                }
+            }
+            accum
+        });
 
     (Solution::from(sol1), Solution::from(sol2))
 }
