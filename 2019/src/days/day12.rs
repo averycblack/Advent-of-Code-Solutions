505e96ad6195f580c3a59b7c682f39beb6cfbafc
Avery Black <avery.c.black@gmail.com>
2025-12-01 21:53:20 -0800

    2019: Remove every day except 18

--- 2019/src/days/day12.rs
+++
@@ -1,8 +0,0 @@
-use aoclib::solution::{Solution, SolutionPair};
-
-pub fn solve(_str: String) -> SolutionPair {
-    let sol1 = 0;
-    let sol2 = 0;
-
-    (Solution::from(sol1), Solution::from(sol2))
-}
