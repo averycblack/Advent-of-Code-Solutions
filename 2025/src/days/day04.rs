b9f9defdd1e887266b7ca00d753c245c9127136f
Avery Black <avery.c.black@gmail.com>
2025-12-03 21:15:21 -0800

    2025: Day 4

--- 2025/src/days/day04.rs
+++ 2025/src/days/day04.rs
@@ -1,8 +1,52 @@
-use aoclib::solution::{Solution, SolutionPair};
+use aoclib::{
+    lib2d::{self, Coordinates, Grid},
+    solution::{Solution, SolutionPair},
+};
 
-pub fn solve(_str: String) -> SolutionPair {
-    let sol1 = 0;
-    let sol2 = 0;
+fn iter(grid: &Grid<char>) -> (u64, Grid<char>) {
+    let mut next = grid.clone();
+    let mut count = 0;
+
+    let max = grid.get_max_size();
+    for x in 0..max.0 {
+        for y in 0..max.1 {
+            let pt = Coordinates(x, y);
+            if grid.get_point(pt).unwrap() != '@' {
+                continue;
+            }
+
+            let rolls = lib2d::DIRS
+                .iter()
+                .filter(|del| grid.get_point(pt + **del).unwrap_or('.') == '@')
+                .count();
+
+            if rolls < 4 {
+                count += 1;
+                next.set_point(pt, '.');
+            }
+        }
+    }
+
+    (count, next)
+}
+
+pub fn solve(str: String) -> SolutionPair {
+    let mut sol2 = 0;
+
+    let grid = Grid::from_string(&str, |c| c);
+    let (sol1, _) = iter(&grid);
+
+    let mut cur_grid = grid;
+    loop {
+        let (cnt, next) = iter(&cur_grid);
+        sol2 += cnt;
+
+        if cnt == 0 {
+            break;
+        }
+
+        cur_grid = next;
+    }
 
     (Solution::from(sol1), Solution::from(sol2))
 }
